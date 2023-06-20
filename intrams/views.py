37f9c4371477f63785bc912e923c06725929876a
Danica Mae Maceda <128012749+Danicaaaaaaa@users.noreply.github.com>
2023-06-20 15:35:57 +0800

    06-20-23/danicaa)\

--- intrams/views.py
+++ intrams/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def home(request):
+   return render(request, 'accounts/hompe page.html')
+
+def teams(request):
+   return render(request, 'accounts/team.html')
+
+def courses(request):
+   return render(request, 'accounts/courses.html')
+
+def student(request):
+   return render(request, 'accounts/student.html')
